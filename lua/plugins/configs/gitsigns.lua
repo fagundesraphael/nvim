431dfff2e8dff9f05792db9725cd081e3a85efb4
Raphael Fagundes <fagundesraphael7@gmail.com>
2025-03-27 20:47:13 -0400

    add gitsigns icons

--- lua/plugins/configs/gitsigns.lua
+++ lua/plugins/configs/gitsigns.lua
@@ -1,11 +1,19 @@
 local options = {
   signs = {
-    add = { text = "│" },
-    change = { text = "│" },
+    add = { text = " " },
+    change = { text = "󱗜" },
     delete = { text = "󰍵" },
-    topdelete = { text = "‾" },
-    changedelete = { text = "~" },
-    untracked = { text = "│" },
+    topdelete = { text = "󱥨 " },
+    changedelete = { text = "󰾟 " },
+    untracked = { text = "󰰧 " },
+  },
+  signs_staged = {
+    add = { text = " " },
+    change = { text = "󱗜" },
+    delete = { text = "󰍵" },
+    topdelete = { text = "󱥨 " },
+    changedelete = { text = "󰾟 " },
+    untracked = { text = "󰰧 " },
   },
 
   on_attach = function(bufnr)
